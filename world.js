798271079197136a92894f29d4f233d79727d2d8
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2023-11-21 01:39:42 -0500

    Modified

--- world.js
+++ world.js
@@ -18,42 +18,38 @@ document.addEventListener('DOMContentLoaded', function () {
     function displayResults(results, lookupType) {
         var resultDiv = document.getElementById('result');
         resultDiv.innerHTML = '';
-    
-        if (results.length > 0) {
-            if ('CityName' in results[0]) {
-                var table = document.createElement('table');
-                table.classList.add('city-table');
-    
-                var headerRow = table.insertRow(0);
-                for (var key in results[0]) {
-                    var th = document.createElement('th');
-                    th.textContent = key.replace(/_/g, ' ').charAt(0).toUpperCase() + key.slice(1);
-                    headerRow.appendChild(th);
-                }
-    
-                results.forEach(function (row) {
-                    var tr = table.insertRow();
-                    for (var key in row) {
-                        var cell = tr.insertCell();
-                        cell.textContent = row[key];
-                    }
-                });
-    
-                resultDiv.appendChild(table);
-            } else {
-                var ul = document.createElement('ul');
-                ul.classList.add('country-list');
-    
-                results.forEach(function (row) {
-                    var li = document.createElement('li');
-                    li.textContent = row.name + ' is ruled by ' + row.head_of_state;
-                    ul.appendChild(li);
-                });
-    
-                resultDiv.appendChild(ul);
+
+        if (results.length > 0 && results[0].cityName) {
+            var table = document.createElement('table');
+            table.classList.add('city-table');
+
+            var headerRow = table.insertRow(0);
+            for (var key in results[0]) {
+                var th = document.createElement('th');
+                th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+                headerRow.appendChild(th);
             }
+
+            results.forEach(function (row) {
+                var tr = table.insertRow();
+                for (var key in row) {
+                    var cell = tr.insertCell();
+                    cell.textContent = row[key];
+                }
+            });
+
+            resultDiv.appendChild(table);
         } else {
-            resultDiv.textContent = 'No results found.';
+            var ul = document.createElement('ul');
+            ul.classList.add('country-list');
+
+            results.forEach(function (row) {
+                var li = document.createElement('li');
+                li.textContent = row.name + ' is ruled by ' + row.head_of_state;
+                ul.appendChild(li);
+            });
+
+            resultDiv.appendChild(ul);
         }
-    }    
+    }
 });
